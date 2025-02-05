01e18c6df0951adcc04220be5e0c39228f475cf9
iara-elise <iara.elise.0006@gmail.com>
2025-02-04 17:53:25 -0800

    Primeiro commit.

---
+++ Projeto-01/index.js
@@ -0,0 +1,28 @@
+function iniciarContagem() {
+
+    tempo = setInterval(function() {
+
+        let cronometro = document.getElementById('contagem').innerHTML;
+        let contagemRegressiva = parseInt(cronometro) - 1;
+
+        if (contagemRegressiva === 0) {
+
+            document.getElementById('contagem').innerHTML = `
+            
+                <p> Passando para lembrá-lo que eu te amo! </p> <br>
+                <p> Seja sempre a pessoa cativante que é. Assim, você conquista o mundo. </p>
+            `;
+
+            pararContagem();
+        }
+
+        else {
+
+            document.getElementById('contagem').innerHTML = contagemRegressiva;
+        }
+    }, 1000)
+}
+
+function pararContagem () {
+    clearInterval(tempo);
+}
